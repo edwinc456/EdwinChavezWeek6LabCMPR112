ea86695ae6d4769eccb33b6ca45ff681ddbe985d
edwinc456 <skydude51@yahoo.com>
2014-03-23 20:11:53 -0700

    Rock ,paper, scissors

---
+++ Week6Lab/src/week6lab.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class week6lab 
+{
+public static void main(String[]args)
+	{
+	 int computerNum;
+	 int num2;
+	 Scanner input = new Scanner(System.in);
+	 System.out.println("Press 1 for rock, 2 for paper, 3 for scissors: ");
+	 num2=input.nextInt();
+	 computerNum= 0 + (int)(Math.random()*3);
+	 if(computerNum == num2)
+	 {
+		 System.out.printf("It has been a draw! both of you selected %d! Thank you for playing!" , computerNum);
+		 
+	 }
+	 else if(computerNum<num2)
+	 {
+		 System.out.printf("Your opponent chose %d. Thank you for playing!",computerNum);
+		
+	 }
+	 else if(computerNum>num2)
+	 {
+		 System.out.printf("Your opponent chose %d. Thank you for playing!", computerNum );
+	 }
+	}
+}
